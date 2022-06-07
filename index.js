75963137fec54a2c75432c47ab676afcf18073b9
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-07 19:05:48 +0300

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Nick Ndolo';
+let welcomeMessage = "Welcome to Flatbook, " + `${currentUser}!`
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "NICK NDOLO!"
+
+let shortGreeting = "Welcome, " + "Nick!"
